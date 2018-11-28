5ff84189ad1926120eaaa6f107aa153f1d4f7b55
ag05806 <ag05806@georgiasouthern.edu>
2018-11-27 19:44:29 -0500

    Create getAttachmentTypes.js

---
+++ lambda/getAttachmentTypes.js
@@ -0,0 +1,67 @@
+var mysql = require('./node_modules/mysql');
+var config = require('./config.json');
+var validator = require('./validation.js');
+
+function formatErrorResponse(code, errs) {
+	return JSON.stringify({ 
+		error  : code,
+		errors : errs
+	});
+}
+
+exports.handler = (event, context, callback) => {
+	//instruct the function to return as soon as the callback is invoked
+	context.callbackWaitsForEmptyEventLoop = false;
+
+	//validate input
+	var errors = new Array();
+	
+	
+	if(errors.length > 0) {
+		// This should be a "Bad Request" error
+		callback(formatErrorResponse('BAD_REQUEST', errors));
+	} else {
+	
+		//getConnection equivalent
+		var conn = mysql.createConnection({
+			host 	: config.dbhost,
+			user 	: config.dbuser,
+			password : config.dbpassword,
+			database : config.dbname
+		});
+		
+		//attempts to connect to the database
+		conn.connect(function(err) {
+		  	
+			if (err)  {
+      // This should be a "Internal Server Error" error
+				callback(formatErrorResponse('INTERNAL_SERVER_ERROR', [err]));
+			} else {
+				console.log("Connected!");
+				var sql = "SELECT attachmenttypeid, name, extension FROM attachmenttypes ORDER BY name";
+				
+				conn.query(sql, [event.attachmenttypeid, event.extension], function (err, result) {
+				  	if (err) {
+						// This should be a "Internal Server Error" error
+						callback(formatErrorResponse('INTERNAL_SERVER_ERROR', [err]));
+				  	} else {
+				  		// Pull out just the codes from the "result" array (index '1')
+				  		var codes = [];
+				  		for(var i=0; i<result.length; i++) {
+							codes.push(result[i]['registrationcode']);
+						}
+						// Build an object for the JSON response with the userid and reg codes
+						var json = { 
+							attachmenttypeid : event.attachmenttypeid,
+							name : event.name,
+							extension : event.extension
+						};
+						// Return the json object
+						callback(null, json);
+						//conn.exit();
+				  	}
+				}); 
+			} // no connection error
+		}); //connect database
+	} //no validation errors
+} //handler
